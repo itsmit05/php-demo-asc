4eac241b7e65770c41b8606ff48f6394396ffa4d
itsmit05 <smitrana05@gmail.com>
2025-05-29 08:01:31 +0000

    Initial commit: Add index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Demo</title>
+</head>
+<body>
+    <h1>Hello from PHP!</h1>
+    <p>The current date and time is: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
